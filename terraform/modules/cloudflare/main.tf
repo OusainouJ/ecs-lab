23f068de9d0edae86ee38f4506118022288c4180
Ousainou <ousainou.joof@gmail.com>
2025-04-01 19:36:49 +0100

    cloudflare fix

--- terraform/modules/cloudflare/main.tf
+++ terraform/modules/cloudflare/main.tf
@@ -4,9 +4,18 @@ provider "cloudflare" {
 
 resource "cloudflare_record" "alb_dns" {
   zone_id = var.cloudflare_zone_id
-  name    = var.cloudflare_record_name
-  value   = var.alb_dns_name
+  name    = var.cloudflare_record_name  # The subdomain
+  content   = var.alb_dns_name  # ALB DNS Name from AWS
   type    = "CNAME"
   ttl     = 300
-  proxied = true
+  proxied = true  
+}
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"  
+    }
+  }
 }
