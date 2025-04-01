48db75457e77646dfc527c935da8599030301c3c
Ousainou <ousainou.joof@gmail.com>
2025-04-01 13:42:40 +0100

    specify cloudflare/cloudflare in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,3 +52,16 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
+# tells terraform that cloudflare comes from cloudflare/cloudflare and not hashicorp/cloudflare
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
